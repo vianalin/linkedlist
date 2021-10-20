8ffc81a2a35c538bf9815d70ce63db430e457993
Viana Lin <vianalin@pop-os.localdomain>
2021-10-19 23:34:25 -0400

    main

---
+++ main.c
@@ -0,0 +1,14 @@
+int main() {
+	srand(time(NULL));
+	
+	char *food = "french fries";
+	double price = rand() / 1000000000.0;
+	//bill(a);
+	
+	//struct order *b = insert_front(a, food, price);
+	//print_list(a);
+	//free_list(a);
+	//bill(a);
+	
+	return 0;
+}
