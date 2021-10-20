aa2b383027f5fb9e43b3666b16819959634ad8cf
Viana Lin <vianalin@pop-os.localdomain>
2021-10-20 00:22:52 -0400

    remove

--- structy2.h
+++ structy2.h
@@ -9,4 +9,4 @@ void print_bill(struct order *o);
 void print_list(struct order *o);
 struct order * insert_front(struct order * current, char * food, double price);
 struct order * free_list(struct order * current);
-struct order * remove_node(struct order * current, double price); 
+struct order * remove_node(struct order * front, char * food, double price);
